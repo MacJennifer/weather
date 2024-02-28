13549e7a71d67a4ce188078f8b7ec472c8818cdc
Jennifer Macaire <macaire.jennifer@sfr.fr>
2024-02-28 10:56:00 +0100

    ajustement ddesign

--- App.js
+++ App.js
@@ -69,13 +69,16 @@ export default function App() {
             <Text style={styles.description}>
               {data.weather && data.weather[0] && data.weather[0].description}
             </Text>
-
             <Text style={styles.tempText}>{`${Math.round(
               data?.main?.temp
             )} °C`}</Text>
             <Text style={styles.minMaxText}>{`Min ${Math.round(
               data?.main?.temp_min
+              //  le ? evite des erreur et affichera undefined
             )} °C / Max ${Math.round(data?.main?.temp_max)} °C`}</Text>
+            <Text style={styles.wind}>
+              Vent : {`${Math.round(data?.wind?.speed)} KM/H`}
+            </Text>
           </View>
         )}
       </ImageBackground>
@@ -113,7 +116,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   cityCountryText: {
-    color: "#00000",
+    color: "#ffff",
     fontSize: 40,
     fontWeight: "bold",
     marginBottom: 10,
@@ -122,6 +125,7 @@ const styles = StyleSheet.create({
     color: "#00000",
     fontSize: 22,
     marginVertical: 10,
+    marginTop: 30,
   },
   tempText: {
     fontSize: 45,
@@ -131,14 +135,22 @@ const styles = StyleSheet.create({
   },
   minMaxText: {
     fontSize: 25,
-    color: "#00000",
+    color: "#ffff",
     marginVertical: 10,
     fontWeight: "500",
+    marginTop: 30,
     marginBottom: 20,
   },
   description: {
     fontSize: 45,
-    fontWeight: "500",
+    fontWeight: "800",
     textAlign: "center",
+    color: "#00000",
+  },
+  wind: {
+    fontSize: 25,
+    color: "#ffff",
+    marginTop: 30,
+    fontWeight: "500",
   },
 });
